bcffab673ea52be10353332d96142e7cb2a94a3f
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-02-15 21:44:35 -0800

    Refactor the database connection

--- src/database/DatabaseConnection.java
+++ src/database/DatabaseConnection.java
@@ -7,11 +7,16 @@ import java.sql.SQLException;
 public class DatabaseConnection {
 
 	public Connection connect() {
-		String path = System.getProperty("user.dir"); // Get current local directory
-		String url = "jdbc:sqlite:" + path + "/src/database/MindPrimeDB.db"; // SQLite url para hacer conexion
+		
 		Connection conn = null;
+		String path = System.getProperty("user.dir"); // Get current local directory
+		
 		try {
+			String url = "jdbc:sqlite:" + path + "/src/database/MindPrimeDB.db"; // SQLite url para hacer conexion
 			conn = DriverManager.getConnection(url);
+			
+            System.out.println("Connection to SQLite has been established.");
+            
 		} catch (SQLException e) {
 			System.out.println("Hubo un error al conectarse a la base de datos: " + e.getMessage());
 		}

5126c3d503b5abcda19d03269fd1365ca82f4d7d
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-03-30 09:29:44 -0700

    Deleted unused folders from the fiorellaroma project

--- src/Controllers/FamiliarityInstructionController.java
+++ src/Controllers/FamiliarityInstructionController.java
@@ -1,67 +1,42 @@
 package Controllers;
 
-import LoginAdminApp.MainApp;
-import javafx.animation.KeyFrame;
-import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
-import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-import javafx.util.Duration;
 
 import java.io.IOException;
-import java.net.URL;
-import java.sql.SQLException;
-import java.util.ResourceBundle;
 
-public class FamiliarityInstructionController implements Initializable {
+public class FamilirialityInstructionController {
 
-
-    public Button imageButton;
-
-    Stage familiarityInstructionStage = new Stage();
+    Stage foucsPointStage = new Stage();
     FocusPointController focusPoint = new FocusPointController();
-    FamiliarityWordController familiarityWord = new FamiliarityWordController();
-    PrimingWordController primingWordController = new PrimingWordController();
+   // FamiliarityWordController familiarityWord = new FamiliarityWordController();
 
-    public FamiliarityInstructionController() throws SQLException {
-    }
-
-
-    public void showFamiliarityInstruction() {
+    public void StartFocusPoint(ActionEvent actionEvent) {
         try {
+            FXMLLoader adminLoader = new FXMLLoader();
+            Pane FsmilirislityRoot = adminLoader.load(getClass().getResource("../Layouts/FocusPoint.fxml").openStream());
 
-            FXMLLoader familiarityInstructionLoader = new FXMLLoader();
-            Pane familiarityInstructionRoot = (Pane) familiarityInstructionLoader.load(getClass().getResource("/Layouts/FamiliarityInstruction.fxml").openStream());
-
-//            FamiliarityInstructionController adminController = (FamiliarityInstructionController) familiarityInstructionLoader.getController();
-
-            Scene scene = new Scene(familiarityInstructionRoot);
-            this.familiarityInstructionStage.setScene(scene);
-            this.familiarityInstructionStage.setTitle("familiarityInstruction Point");
-            this.familiarityInstructionStage.setResizable(false);
-            this.familiarityInstructionStage.show();
+            // adminController adminController = (adminController) adminLoader.getController();
+            Scene scene = new Scene(FsmilirislityRoot);
+            foucsPointStage.setScene(scene);
+            foucsPointStage.setTitle("Punto de Enfoque");
+            foucsPointStage.setResizable(false);
+            foucsPointStage.show();
+             focusPoint.setTimer();
 
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
-
-    public void showFocusPoint(ActionEvent actionEvent) throws InterruptedException {
+    public void showFocusPoint(ActionEvent actionEvent) {
         focusPoint.showFocusPoint();
         focusPoint.setTimer();
-//        focusPoint.close();
-
-        Stage stage = (Stage)this.imageButton.getScene().getWindow();
-        stage.close();
     }
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
+    public void closeFamiliarityInstruction(){
+      //  this.familiarityInstructionStage.close();
     }
 
 }

3aa45619d2a25256da809d40400c29e22cd93779
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-03-30 09:34:47 -0700

    Change unused stuff to make database connection work well

--- src/DatabaseConnection/dbConnection.java
+++ src/DatabaseConnection/dbConnection.java
@@ -6,17 +6,21 @@ import java.sql.SQLException;
 
 public class dbConnection {
 
-    public static Connection getConnection() throws SQLException {
+    public static Connection getConnection() {
         Connection conn = null;
         String path = System.getProperty("user.dir");
-        String SQLCONNECTION = "jdbc:sqlite:" + path +"/src/DatabaseConnection/MindPrimeDBCambios.db";
+        String SQLCONNECTION = "jdbc:sqlite:" + path + "/src/DatabaseConnection/MindPrimeDBCambios.db";
 
         try {
-            Class.forName("org.sqlite.JDBC");
-            return DriverManager.getConnection(SQLCONNECTION);
-        } catch (ClassNotFoundException ex) {
+
+//            Class.forName("org.sqlite.JDBC");
+            conn = DriverManager.getConnection(SQLCONNECTION);
+            System.out.println("Connection to SQLite has been established.");
+
+        } catch (SQLException ex) {
+            System.out.println("Hubo un error al conectarse a la base de datos: " + ex.getMessage());
             ex.printStackTrace();
         }
-        return null;
+        return conn;
     }
 }

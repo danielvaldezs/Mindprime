9108330e86f26467398260265251063aef782a47
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-03-30 15:23:37 -0700

    Fix database connection

--- src/DatabaseConnection/dbConnection.java
+++ src/DatabaseConnection/dbConnection.java
@@ -6,20 +6,23 @@ import java.sql.SQLException;
 
 public class dbConnection {
 
+	// Metodo para obtener la conexion a la base de datos
     public static Connection getConnection() {
+    	
         Connection conn = null;
         String path = System.getProperty("user.dir");
         String SQLCONNECTION = "jdbc:sqlite:" + path + "/src/DatabaseConnection/MindPrimeDBCambios.db";
 
-        try {
+        try { // En caso de que se realice la conexion de manera exitosa
 
-//            Class.forName("org.sqlite.JDBC");
+//          Class.forName("org.sqlite.JDBC");
             conn = DriverManager.getConnection(SQLCONNECTION);
             System.out.println("Connection to SQLite has been established.");
 
-        } catch (SQLException ex) {
+        } catch (SQLException ex) { // En caso de que no se realice la conexion
+        	
             System.out.println("Hubo un error al conectarse a la base de datos: " + ex.getMessage());
-            ex.printStackTrace();
+            
         }
         return conn;
     }

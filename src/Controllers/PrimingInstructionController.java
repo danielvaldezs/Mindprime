e2788456fc6bb9cbf5ec13d424ed189a992dc13a
Fernando Hernandez <fernando.hdz97@gmail.com>
2019-04-01 22:53:37 -0700

    Fix primingInstructionController to be able to show the view

--- src/Controllers/PrimingInstructionController.java
+++ src/Controllers/PrimingInstructionController.java
@@ -11,40 +11,45 @@ import java.io.IOException;
 import java.sql.SQLException;
 
 public class PrimingInstructionController {
-	
-	
-		public Button imageButton;
-        Stage primingInstructionStage = new Stage();
-	    FocusPointController focusPoint = new FocusPointController();
-	    PrimingWordController primingWord = new PrimingWordController();
-        
-        public PrimingInstructionController() throws SQLException {
+
+	public Button imageButton;
+    Stage primingInstructionStage = new Stage();
+    FocusPointController focusPoint = new FocusPointController();
+    PrimingWordController primingWord = new PrimingWordController();
+    
+    public PrimingInstructionController() throws SQLException {
+    	
+    }
+
+
+    public void showPrimingInstruction() {
+        try {
+
+            FXMLLoader primingInstructionLoader = new FXMLLoader();
+            Pane primingInstructionRoot = (Pane) primingInstructionLoader.load(getClass().getResource("/Layouts/PrimingInstruction.fxml").openStream());
+
+//            AdminController adminController = (AdminController) primingInstructionLoader.getController();
+            Scene scene = new Scene(primingInstructionRoot);
+            this.primingInstructionStage.setScene(scene);
+            this.primingInstructionStage.setTitle("Priming Instruction Point");
+            this.primingInstructionStage.setResizable(false);
+            this.primingInstructionStage.show();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+    }
+    
+    public void showFocusPoint(ActionEvent actionEvent) throws InterruptedException {
+        focusPoint.showFocusPoint();
+        focusPoint.setTimer2();
+//        focusPoint.close();
 
-        public void showPrimingInstruction() {
-            try {
+        Stage stage = (Stage)this.imageButton.getScene().getWindow();
+        stage.close();
+    }
 
-                FXMLLoader primingInstructionLoader = new FXMLLoader();
-                Pane primingInstructionRoot = (Pane) primingInstructionLoader.load(getClass().getResource("/Layouts/PrimingInstruction.fxml").openStream());
-
-//                AdminController adminController = (AdminController) primingInstructionLoader.getController();
-                Scene scene = new Scene(primingInstructionRoot);
-                this.primingInstructionStage.setScene(scene);
-                this.primingInstructionStage.setTitle("Priming Instruction Point");
-                this.primingInstructionStage.setResizable(false);
-                this.primingInstructionStage.show();
-
-            } catch (IOException ex) {
-                ex.printStackTrace();
-            }
-        }
-        // TODO: Fix to show Priming Word Activity after Priming Instruction
-        public void showFocusPoint(ActionEvent actionEvent) throws InterruptedException {
-            focusPoint.showFocusPoint();
-            focusPoint.setTimer();
-//            focusPoint.close();
-
-            Stage stage = (Stage)this.imageButton.getScene().getWindow();
-            stage.close();
-        }
+//    public void closeFamiliarityInstruction(){
+//        this.primingInstructionStage.close();
+//    }
 }
